139676b8806356fbafdf9610444838f768b63687
RobertoDip <dip.jesusr@gmail.com>
2013-05-16 16:47:49 -0300

    añadiendo el proyecto

---
+++ MejoresPeliculasGangsters/MejoresPeliculasGangsters/MainPage.xaml.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using System.ComponentModel;
+using System.Windows.Media.Imaging;
+using Microsoft.Phone.Tasks;
+
+namespace MejoresPeliculasGangsters
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Set the data context of the listbox control to the sample data
+            DataContext = App.ViewModel;
+            this.Loaded += new RoutedEventHandler(MainPage_Loaded);
+        }
+
+        // Load data for the ViewModel Items
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (!App.ViewModel.IsDataLoaded)
+            {
+                App.ViewModel.LoadData();
+            }
+        }
+
+        private void controlPivot_LoadingPivotItem(object sender, PivotItemEventArgs e)
+        {
+            switch (this.controlPivot.SelectedIndex)
+            {
+                case 0:
+                    changeBackground("Images/principal.jpg");
+                    break;
+                case 1:
+                    changeBackground("Images/elPadrino.jpg");
+                    break;
+                case 2:
+                    changeBackground("Images/onceUponTime.jpg");
+                    break;
+                case 3:
+                    changeBackground("Images/scarface.jpg");
+                    break;
+                case 4:
+                    changeBackground("Images/ciudadDeDios.jpg");
+                    break;
+                case 5:
+                    changeBackground("Images/carlitosWay.jpg");
+                    break;
+                case 6:
+                    changeBackground("Images/Goodfellas.jpg");
+                    break;
+            }
+
+        }
+
+        private void changeBackground(string str)
+        {
+            Image myImage3 = new Image();
+            BitmapImage bi3 = new BitmapImage();
+            bi3.UriSource = new Uri(str, UriKind.Relative);
+            myImage3.Source = bi3;
+            ImageBrush img = new ImageBrush();
+            img.ImageSource = bi3;
+            this.controlPivot.Background = img;
+        }
+
+        private void FirstListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void elPadrino_Tap(object sender, GestureEventArgs e)
+        {
+            this.controlPivot.SelectedIndex = 1;
+        }
+
+        private void eraseUnaVez_Tap(object sender, GestureEventArgs e)
+        {
+            this.controlPivot.SelectedIndex = 2;
+        }
+
+        private void scarface_Tap(object sender, GestureEventArgs e)
+        {
+            this.controlPivot.SelectedIndex = 3;
+        }
+
+        private void goodFellas_Tap(object sender, GestureEventArgs e)
+        {
+            this.controlPivot.SelectedIndex = 6;
+        }
+
+        private void carlitos_Tap(object sender, GestureEventArgs e)
+        {
+            this.controlPivot.SelectedIndex = 5;
+        }
+
+        private void ciudadDios_Tap(object sender, GestureEventArgs e)
+        {
+            this.controlPivot.SelectedIndex = 4;
+        }
+
+        private void iconMenu_Click(object sender, EventArgs e)
+        {
+            this.controlPivot.SelectedIndex = 0;
+        }
+
+        private void iconContacto_Click(object sender, EventArgs e)
+        {
+            EmailComposeTask emailComposeTask = new EmailComposeTask();
+
+            emailComposeTask.Subject = "message subject";
+            emailComposeTask.Body = "message body";
+            emailComposeTask.To = "rob_bj92@hotmail.com";
+
+            emailComposeTask.Show();
+
+        }
+
+        private void iconAcercaDe_Click(object sender, EventArgs e)
+        {
+            MarketplaceReviewTask marketplaceReviewTask = new MarketplaceReviewTask();
+            marketplaceReviewTask.Show();
+        }
+
+    }
+}
---
+++ MejoresPeliculasGangsters/MejoresPeliculasGangsters/ViewModels/MainViewModel.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+
+
+namespace MejoresPeliculasGangsters
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public MainViewModel()
+        {
+            this.Items = new ObservableCollection<ItemViewModel>();
+        }
+
+        /// <summary>
+        /// A collection for ItemViewModel objects.
+        /// </summary>
+        public ObservableCollection<ItemViewModel> Items { get; private set; }
+
+        private string _sampleProperty = "Sample Runtime Property Value";
+        /// <summary>
+        /// Sample ViewModel property; this property is used in the view to display its value using a Binding
+        /// </summary>
+        /// <returns></returns>
+        public string SampleProperty
+        {
+            get
+            {
+                return _sampleProperty;
+            }
+            set
+            {
+                if (value != _sampleProperty)
+                {
+                    _sampleProperty = value;
+                    NotifyPropertyChanged("SampleProperty");
+                }
+            }
+        }
+
+        public bool IsDataLoaded
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Creates and adds a few ItemViewModel objects into the Items collection.
+        /// </summary>
+        public void LoadData()
+        {
+            // Sample data; replace with real data
+            this.Items.Add(new ItemViewModel() { LineOne = "El Padrino", LineTwo = "Ver más", LineThree = "" });
+            this.Items.Add(new ItemViewModel() { LineOne = "Once upon a time...", LineTwo = "Ver más", LineThree = "" });
+            this.Items.Add(new ItemViewModel() { LineOne = "Scarface", LineTwo = "Ver más", LineThree = "" });
+            this.Items.Add(new ItemViewModel() { LineOne = "Goodfellas", LineTwo = "Ver más", LineThree = "Facilisi faucibus habitant inceptos interdum lobortis nascetur pharetra placerat pulvinar sagittis senectus sociosqu" });
+            this.Items.Add(new ItemViewModel() { LineOne = "Carlito's Way", LineTwo = "Ver más", LineThree = "Facilisi faucibus habitant inceptos interdum lobortis nascetur pharetra placerat pulvinar sagittis senectus sociosqu" });
+            this.Items.Add(new ItemViewModel() { LineOne = "Cidade de Deus", LineTwo = "Ver más", LineThree = "Facilisi faucibus habitant inceptos interdum lobortis nascetur pharetra placerat pulvinar sagittis senectus sociosqu" });
+            this.Items.Add(new ItemViewModel() { LineOne = "Cine de Gangsters", LineTwo = "Ver más", LineThree = "Facilisi faucibus habitant inceptos interdum lobortis nascetur pharetra placerat pulvinar sagittis senectus sociosqu" });
+            this.Items.Add(new ItemViewModel() { LineOne = "Contacto", LineTwo = "Consideras que falta alguna pelicula?", LineThree = "Facilisi faucibus habitant inceptos interdum lobortis nascetur pharetra placerat pulvinar sagittis senectus sociosqu" });
+            this.Items.Add(new ItemViewModel() { LineOne = "Acerca de", LineTwo = "copyright y agradecimientos", LineThree = "Facilisi faucibus habitant inceptos interdum lobortis nascetur pharetra placerat pulvinar sagittis senectus sociosqu" });
+
+
+            this.IsDataLoaded = true;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
